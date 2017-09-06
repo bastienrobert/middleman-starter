05192dd31636138c280878633366b1fc6fa422f3
Bastien <bastienrbt@protonmail.com>
2017-09-07 01:19:45 +0200

    Fix URL helper with subdirectories

---
+++ README.md
@@ -0,0 +1,14 @@
+# Middleman Starter
+Starter for middleman, using i18n and including a URL helper to translate the same page in a different language.
+
+## URL helper
+To translate the same page in a different language.
+By default, :fr is the default_locale. It works with any other default locale or within it. These are the steps to setup :
+
+- **config.rb :** precise your default_locale in mount_as_root
+- **locales *.yml :** for each locale, precise :
+  - the title _title_ of the page (identifier)
+  - the title translation : in locale.titles
+  - subdirectories _dir_ in a list (URL direction)
+  - subdirectories translation : in locale.paths (one by one, not a list)
+- **views :** really simple, call `<%= link_translate(:fr) %>` to translate in `:fr`, do the same with `:en` to translate in `:en`
--- helpers/url_helper.rb
+++ helpers/url_helper.rb
@@ -1,24 +1,30 @@
 module URLHelper
 
   def link_translate(lang)
-    current_URI = URI(current_page.url)
-                  .to_s
-                  .split('/')
-
-    if I18n.locale != I18n.default_locale
-      current_URI.shift(2)
-      current_URI = current_URI.join('/')
-    else
-      current_URI.shift(1)
-      current_URI = current_URI.join('/')
-    end
-
     return link_to data.languages.send(lang.to_s),
     if lang === I18n.default_locale
-      config[:host] + '/' + I18n.translate!("paths.#{current_page.data.title}", locale: lang)
+      config[:host] + '/' +
+      path_translate(lang) +
+      title_translate(lang)
     else
-      config[:host] + '/' + lang.to_s + '/' + I18n.translate!("paths.#{current_page.data.title}", locale: lang)
+      config[:host] + '/' +
+      lang.to_s + '/' +
+      path_translate(lang) +
+      title_translate(lang)
     end
+
+    def path_translate(lang)
+      begin
+        current_page.data.dir.map { |dir| t("paths.#{dir}", locale: lang, :default => dir) + '/'}.join('')
+      rescue
+        return ''
+      end
+    end
+
+    def title_translate(lang)
+      t("paths.#{current_page.data.title}", locale: lang, :default => current_page.data.title)
+    end
+
   end
 
 end
